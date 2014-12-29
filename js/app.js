85c97e46df105faf21939aab459aaf90212128f2
shgtkshruch <shgtk.shruch@gmail.com>
2014-12-29 11:38:30 +0900

    Render list element with template.

--- js/app.js
+++ js/app.js
@@ -16,14 +16,18 @@
 
   var TaskView = Backbone.View.extend({
     tagName: 'li',
-    className: 'liClass',
-    id: 'liId'
+    template: _.template('<%- title %>'),
+    render: function () {
+      var template = this.template(this.model.toJSON());
+      this.$el.html(template);
+      return this;
+    }
   });
 
   var taskView = new TaskView({
     model: task
   });
 
-  console.log(taskView.el);
+  console.log(taskView.render().el);
 
 }());
